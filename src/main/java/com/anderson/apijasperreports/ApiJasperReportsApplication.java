72473983157a5f824aa5609795cf0f3795e2b445
asc <silvaandersonpi@gmail.com>
2022-04-14 09:33:07 -0300

    commit initial

---
+++ src/main/java/com/anderson/apijasperreports/ApiJasperReportsApplication.java
@@ -0,0 +1,13 @@
+package com.anderson.apijasperreports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiJasperReportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiJasperReportsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anderson/apijasperreports/ApiJasperReportsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anderson.apijasperreports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiJasperReportsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
