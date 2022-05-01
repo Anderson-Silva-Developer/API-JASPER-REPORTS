6374429023c3d41ec8703143bba8de31e6a6535e
asc <silvaandersonpi@gmail.com>
2022-04-30 22:05:48 -0300

    configuração de conexão

--- src/main/java/com/anderson/apijasperreports/controller/Reports.java
+++ src/main/java/com/anderson/apijasperreports/controller/Reports.java
@@ -1,19 +1,24 @@
-package com.anderson.apijasperreports.controllers;
+package com.anderson.apijasperreports.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.Connection;
+
 @RestController
 @RequestMapping("/reports")
 public class Reports {
 
+    @Autowired
+    private Connection connection;
+
     @GetMapping("/hello")
     @ResponseBody
     public String helloJasperReports(){
-
-        return "JASPER-REPORTS";
+        return connection!=null?"JASPER-REPORTS":"Erro de conexão";
     }
 
 }
